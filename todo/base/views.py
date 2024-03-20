6e1914500a9d73d69e3f7a0ac3adcf7946fecd1b
HastagKc <chaudharykshittiz950@gmail.com>
2024-03-20 14:54:51 +0545

    todo project using django framework

---
+++ todo/base/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Todo
+
+# Import necessary modules
+from django.shortcuts import render
+from .models import Todo
+
+
+# Define the view function for the homepage
+def home(request):
+    # Fetch all Todo objects from the database
+    todos = Todo.objects.all()
+
+    # Create a dictionary containing the fetched todos
+    content = {"todos": todos}
+
+    # Render the 'index.html' template with the todos passed in the context
+    return render(request, "index.html", context=content)
+
+
+# defining the view function for the createpage
+def create(request):
+    if request.method == "POST":
+        # print(request.POST)
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        status = request.POST.get("status")
+        Todo.objects.create(name=name, description=description, status=status)
+        return redirect("home")
+    return render(request, "create.html")
