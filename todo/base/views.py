5a74d8229ee3e73716cf7a86b60b2fee5fc9a737
HastagKc <chaudharykshittiz950@gmail.com>
2024-03-22 20:05:07 +0545

    Complete

--- todo/base/views.py
+++ todo/base/views.py
@@ -7,10 +7,8 @@ from .models import Todo
 def home(request):
     # Fetch all Todo objects from the database
     todos = Todo.objects.all()
-
     # Create a dictionary containing the fetched todos
     content = {"todos": todos}
-
     # Render the 'index.html' template with the todos passed in the context
     return render(request, "index.html", context=content)
 
@@ -30,5 +28,28 @@ def create(request):
 # defining the view for edit page
 def edit(request, pk):
     todo = Todo.objects.get(id=pk)
+    if request.method == "POST":
+        name = request.POST.get("name")
+        des = request.POST.get("description")
+        status = request.POST.get("status")
+        todo.name = name
+        todo.description = des
+        todo.status = status
+        todo.save()
+        return redirect("home")
     content = {"todo": todo}
     return render(request, "edit.html", context=content)
+
+
+from django.contrib import messages
+
+
+def delete(request, pk):
+    try:
+        todo = Todo.objects.get(id=pk)
+        todo.delete()
+        messages.success(request, "Todo item deleted successfully.")
+    except Todo.DoesNotExist:
+        messages.error(request, "Todo item does not exist.")
+
+    return redirect("home")
